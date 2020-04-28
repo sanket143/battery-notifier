45dbdc4e53bf4e8a1388404ce19b8f401834c3b4
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-04-28 23:00:31 +0530

    Show Notification

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-extern crate battery;
-
+use battery::units;
+use notify_rust::Notification;
 use std::io;
 use std::thread;
 use std::time::Duration;
@@ -19,8 +19,19 @@ fn main() -> battery::Result<()> {
     };
 
     loop {
-        println!("{:?}", battery.state_of_charge());
+        show_notification(&battery.state_of_charge());
         thread::sleep(Duration::from_secs(5));
         manager.refresh(&mut battery)?;
     }
+}
+
+fn show_notification(ratio: &units::Ratio) {
+    let mut _body = String::from("Current Battery: ");
+    println!("{:?}", ratio);
+
+    Notification::new()
+        .summary("Lisa")
+        .body("hello")
+        .show()
+        .unwrap();
 }
