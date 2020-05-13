1a49f39e1b577e77025488505519af5c429cb241
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-05-13 17:30:12 +0530

    Updated instructions

--- docs/install.sh
+++ docs/install.sh
@@ -3,13 +3,13 @@ SYSTEM_BIN="`systemd-path user-binaries`"
 
 mkdir -p $HOME/.config
 mkdir -p $CONFIG_DIR
-mkdir $SYSTEM_BIN
+mkdir -p $SYSTEM_BIN
 
 cd $CONFIG_DIR
-wget https://sanket143.github.io/battery-notifier/battery_notifier_s.service
 wget https://sanket143.github.io/battery-notifier/config.json
-wget https://github.com/sanket143/battery-notifier/releases/download/v0.1.0/battery_notifier_s.tar.gz
+wget https://github.com/sanket143/battery-notifier/releases/download/v0.1.1/battery_notifier_s.tar.gz
 
 tar -xvf battery_notifier_s.tar.gz
-cp battery_notifier $SYSTEM_BIN
+mv battery_notifier $SYSTEM_BIN
 
+rm battery_notifier_s.tar.gz
